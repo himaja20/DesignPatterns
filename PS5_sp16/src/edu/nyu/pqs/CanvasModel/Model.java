016d32352f072bd0046fea112827330e0502ed79
himaja rachakonda <himaja rachakonda>
2016-05-15 02:50:56 -0400

    adding paint logic to model

--- PS5_sp16/src/edu/nyu/pqs/CanvasModel/Model.java
+++ PS5_sp16/src/edu/nyu/pqs/CanvasModel/Model.java
@@ -5,18 +5,33 @@ import java.util.List;
 
 public class Model {
   private List<Listener> listeners = new ArrayList<Listener>();
-  boolean currentState = false;
+  private int curX;
+  private int curY;
+  
+  public Model(){
+    setCurX(-1);
+    setCurY(-1);
+  }
   
   public void startCanvas(){
     fireStartCanvas();
   }
   
-  public void somethingDrawn(int x, int y, int w, int h){
+  public void somethingDrawn(int x, int y){
+    if(getCurX() == -1){
+      setCurX(x);
+      setCurY(y);
+    }
+    fireEventPaint(x, y, 20, 20);
+    setCurX(x);
+    setCurY(y);
+  }
+  
+  public void fireEventPaint(int x, int y, int w, int h){
     for(Listener listener : listeners){
       listener.paint(x,y,w,h);
     }
   }
-  
   public void fireStartCanvas(){
     for(Listener listener : listeners){
       listener.startCanvas();
@@ -29,6 +44,22 @@ public class Model {
   
   public void removeListener(Listener listener){
     listeners.remove(listener);
+  }
+
+  public int getCurX() {
+    return curX;
+  }
+
+  private void setCurX(int x) {
+    this.curX = x;
+  }
+
+  public int getCurY() {
+    return curY;
+  }
+
+  private void setCurY(int y) {
+    this.curY = y;
   }
   
   
